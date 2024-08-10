2db503fd8d7a22613fd519c72f77ed52f7e108ff
lexlam1524 <lamkachun1524@gmail.com>
2024-08-10 18:05:07 +1000

    create playerClass
    create the move function inside the playerclass

--- player.h
+++ player.h
@@ -1,1 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+class Player {
+public:
+    Player(float x, float y, float speed);
+    void move_right();
+    void move_left();
+    float get_x();
+    float get_y();
+
+private:
+    float x, y; // player position
+    float speed; // player movement speed
+};
+
+#endif  // PLAYER_H
